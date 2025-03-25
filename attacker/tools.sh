cf176d72e1d2f592569e939e9bd9a22e6aabcf67
husseih8 <husseih8@gmail.com>
2025-03-25 17:27:26 +0000

    updating tools.sh

--- attacker/tools.sh
+++ attacker/tools.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 echo "[+] Running simulated attack on web server..."
-nmap -sS webserver -p 80 -Pn
-curl http://webserver:80 
+nmap -sS nginx-server1 -p 80 -Pn
+curl http://nginx-server1:80 
 echo "[+] Done. "
 
 # Just to get some logs we can use, a basic simulation of a port scan
