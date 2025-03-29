36a29ec03c59e50d42c00d7607efa59d0fede9a2
Hamse Hussein <34058348+husseih8@users.noreply.github.com>
2025-03-29 20:43:49 +0000

    Delete mini-detection-lab directory

--- mini-detection-lab/adversary/simple-scan.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "[+] Running simulated attack on web server..."
-nmap -sS webserver -p 80 -Pn
-curl http://webserver:80 
-echo "[+] Done. "
-
-# Just to get some logs we can use, a basic simulation of a port scan
